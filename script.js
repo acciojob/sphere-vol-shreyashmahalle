ddffb7cc11353b067fc9ff5a98d582fe4dcba949
shreyashmahalle <128777174+shreyashmahalle@users.noreply.github.com>
2025-09-25 11:12:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+e.preventDefault(); // stop form from refreshing
+    
+    let radius = document.getElementById("radius").value;
+    radius = parseFloat(radius);
+
+    if (isNaN(radius) || radius < 0) {
+        alert("Please enter a valid non-negative number for radius.");
+        return;
+    }
+
+    let volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+    document.getElementById("volume").value = volume.toFixed(4);
+}
+
+
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
